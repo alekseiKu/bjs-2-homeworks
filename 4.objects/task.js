d08f353f57e6aefbd0a60962beecef1e0042af26
Aleksei <67807090+alekseiKu@users.noreply.github.com>
2023-09-30 21:33:34 +0300

    dz4 is done task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.marks) {
+        for (const mark of marks) {
+            this.marks.push(mark);
+        }
+    }
 }
 
 Student.prototype.getAverage = function () {
+    if (!this.marks || this.marks.length === 0) {
+        return 0;
+      }
   
+    let sumOfMarks = this.marks.reduce((acc, number) => acc + number);
+    return sumOfMarks / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
+
+let student1 = new Student("Василиса", "женский", 19);
+let student2 = new Student("Артём", "мужской", 25);
